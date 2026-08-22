43c754acd829234cf5bc12f74ee8f6a777c8a0cd
Kamilly Nascimento <kamillykauany@gmail.com>
2026-08-22 23:12:52 +0000

    Questão 1 finalizada

---
+++ Q1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Filme filme1 = new Filme("Interestelar", "Ficção Científica", 2014);
+        Filme filme2 = new Filme("Toy Story", "Animação", 1995);
+
+        System.out.println(filme1.titulo + "  " + filme1.genero + "  " + filme1.anoLancamento);
+        System.out.println(filme2.titulo + "  " + filme2.genero + "  " + filme2.anoLancamento);
+    }
+}
